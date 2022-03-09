46765d2559761a694cb3d1509e978536954f8b37
Stevolisis <stevolisijoseph@gmail.com>
2022-03-10 12:23:26 +1400

    github comeback

---
+++ home.php
@@ -0,0 +1,572 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Vern Data</title>
+	 <script src="static/jquery-3.5.1.js"></script>
+
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+  <link id="pagestyle" rel='stylesheet' type='text/css'href='static/home2.css'>
+<meta name="viewport"
+content="width-device-width,initial-scale=1.0">
+</head>
+<body>
+<div class="body">
+	<header>
+	<div class="logo">
+		<img src="media/verndev2.svg">
+	</div>
+
+	<div class="nav">
+
+   <div class='bar'><i class="fas fa-bars fa-2x"></i></div>
+   <div class='bar2'><i class="fas fa-bars fa-2x"></i></div>
+
+<div class="nav2">
+	<a href='#Home'>Home</a>
+<a href='#About'>About Us</a>
+<a href='#courses'>Courses</a>
+<a href='#contact'>Contact</a>
+<a href='#signup' class="ctn" >Get Started</a>
+</div>
+	</div>
+
+</header>
+<center class='slide'>
+
+<a href='#home'>Home</a><br>
+<a href='#About'>About Us</a><br>
+<a href='#Process'>Process</a><br>
+<a href='/contact.php'>Contact</a><br>
+<a href='/apply.php'>Apply Now</a>
+</center>
+
+<section class="sec1">
+	<div class="slogan">
+		<div class="slogan2">
+			<h1>Learning Web Development At Ease</h1>
+		</div>
+		<div class="button">
+			<button>Get Started</button>
+		</div>
+	</div>
+	<div class="backimage">
+	<img src="media/backgroundimage4.png">
+	</div>
+</section>
+
+<section class="sec2">
+	<div>
+	<img src="media/github.png" class="img1">
+	<img src="media/w3schools.png" class="img2">
+	<img src="media/freecodecamp2.png" class="img3">
+	<img src="media/sololearn.png" class="img4">
+
+	</div>
+</section>
+
+<section class="sec3">
+
+<div class="secheads">
+	<h3>Our Learning Oppurtunities</h3>
+</div>	
+
+<div class="services">
+
+	<div class="subservices">
+		<div class="icon">
+				<img src="media/front-end.png" class="img4">
+		</div>
+        <div class="info">
+        	<h4>Frontend</h4><br>
+
+        </div>
+        <div class="info2">
+        	<p>Learn frontend development like html, css, javascript, bootstrap at your own convenience</p>
+        </div>
+	</div>
+
+
+
+	<div class="subservices">
+		<div class="icon">
+				<img src="media/hierarchy.png" class="img4">
+		</div>
+        <div class="info">
+        	<h4>Backend</h4><br>
+
+        </div>
+        <div class="info2">
+        	<p>Get your hands dirty with powerful backend languages such as php, nodejs, django and many more.</p>
+        </div>
+	</div>
+
+
+
+	<div class="subservices">
+		<div class="icon">
+		<img src="media/data-transfer.png" class="img4">
+		</div>
+        <div class="info">
+        	<h4>Database</h4><br>
+
+        </div>
+        <div class="info2">
+        	<p>Learn database queries and management without hustles. Learn more about Mysql, Mongodb, perndb and many more.</p>
+        </div>
+	</div>
+
+
+
+	<div class="subservices">
+		<div class="icon">
+		<img src="media/client-server.png" class="img4">
+		</div>
+        <div class="info">
+        	<h4>Hosting</h4><br>
+
+        </div>
+        <div class="info2">
+        	<p>Hosting your site without difficulties, verndev guides through the journey of web hosting so you dont have to make any mistakes.</p>
+        </div>
+	</div>
+
+</div>
+
+</section>
+
+<div class="subheads">
+	<h1>Why E-learning With VernDev</h1>
+</div>
+
+
+<section class="sec4">
+
+	<div class="twocolumn2" id="twocolumn">
+
+		
+		<div class="column">
+			<h1>Flexibility Of Learning</h1>
+			<p>Being a student, I understand that there are some extremely difficult subjects that require extra dedication.Otherwise, the chances of flunking become higher!And when we speak of traditional classroom setup, teaching doesn’t give much flexibility.
+Unfortunately, the teachers are bound by their timings and you have limited time to grasp the subject.This barrier doesn’t exist with the digital classroom setup. With the e-learning software, one gets an ample amount of time to play the recorded lessons repeatedly until understanding the concept.</p>
+		</div>
+
+		<div>
+		<img src="media/backgroundimage11.svg" class="img">
+		</div>
+
+	</div>
+
+
+	<div class="twocolumn">
+
+		<div>
+		<img src="media/newimg2.png" class="img">
+		</div>
+
+
+
+<div class="column">
+			<h1>Access To Vast E-Content</h1>
+			<p>Unlike the conventional classroom, the online teaching classes equipped with advanced e-learning software serve you a number of options.
+The learning process can be made smooth with more relevant & fact-based e-content.With the online educational ERP software, you get access to a vast range of e-content & study materials including e-books, video lessons, audio files, PDFs, PPTs, and personalized e-notes by your teacher. You can make a choice of what you want to study & begin learning the curriculum at your convenience.</p>
+		</div>
+
+	</div>
+
+
+
+	<div class="twocolumn">
+
+
+<div class="column">
+			<h1>Learning At Own Pace</h1>
+			<p>Ever faced the situation when you are writing down the notes by copying what the teacher has explained on the blackboard and even before you finish writing -
+Ah! The teachers just rub off everything! Tada! It’s gone!
+Feels like I have lost a race.With the online learning management system (LMS), such moments of missing out on important learning notes are not concern-worthy. As a student, you can record the video lessons of your teacher & play them repeatedly for developing a better understanding of the topic.</p>
+		</div>
+
+
+		<div>
+		<img src="media/backgroundimage2.png" class="img6">
+		</div>
+
+
+	</div>
+
+
+<div class="twocolumn" style="padding-top: 0px;">
+
+<div>
+		<img src="media/newimg1.png" class="img6">
+		</div>
+
+
+<div class="column">
+			<h1>Online Assessments & Mock Tests</h1>
+			<p>Before you appear for the final online examinations, it’s important that you practice.
+Practice enough to score well during the final take – without facing any technical problem.What I mean is – one may get nervous or face challenges to operate the online assessment platform. With the mock test provision, you can get familiar with the ERP system along with practicing for the final exams.Thus, a learning management system (LMS) integrated with an online test platform can help you prepare confidently – which is a huge advantage of e-learning!</p>
+		</div>
+
+
+		
+
+	</div>
+
+
+
+
+</section>
+
+
+
+
+<section class="sec5">
+<div class="secheads2">
+	<h2>Courses</h2>
+</div>
+<div class="overflow">
+
+		<div class="box">
+		<div class="boximg">
+			<img src="media/learninghtml2.jpg">
+		</div>
+		<div class="content">
+			<h2>Learning Html from Scratch</h2>
+		</div>
+		<div class="content2">
+			<p>Explore the power of html in web development</p>
+		</div>
+
+
+
+		<div class="stars">
+
+			<div class="stars2">
+				<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			</div>
+
+			<div class="stars3">
+				<p>Get Started</p>
+				<a href="/pro/home.php">
+					<i class="fa fa-arrow-circle-right" style="color:#F7931E"></i>
+				</a>
+			</div>
+		</div>
+
+
+	</div>
+
+
+
+		<div class="box">
+		<div class="boximg">
+			<img src="media/learningcss.png">
+		</div>
+		<div class="content">
+			<h2>Learning Html from Scratch</h2>
+		</div>
+		<div class="content2">
+			<p>Explore the power of html in web development</p>
+		</div>
+
+
+
+		<div class="stars">
+
+			<div class="stars2">
+				<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			</div>
+
+			<div class="stars3">
+				<p>Get Started</p>
+				<a href="/pro/home.php">
+					<i class="fa fa-arrow-circle-right" style="color:#F7931E"></i>
+				</a>
+			</div>
+		</div>
+
+
+	</div>
+
+
+
+		<div class="box">
+		<div class="boximg">
+			<img src="media/learningjs.jpeg">
+		</div>
+		<div class="content">
+			<h2>Learning Html from Scratch</h2>
+		</div>
+		<div class="content2">
+			<p>Explore the power of html in web development</p>
+		</div>
+
+
+
+		<div class="stars">
+
+			<div class="stars2">
+				<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			</div>
+
+			<div class="stars3">
+				<p>Get Started</p>
+				<a href="/pro/home.php">
+					<i class="fa fa-arrow-circle-right" style="color:#F7931E"></i>
+				</a>
+			</div>
+		</div>
+
+
+	</div>
+
+
+
+		<div class="box">
+		<div class="boximg">
+			<img src="media/learningnodejs2.jpg">
+		</div>
+		<div class="content">
+			<h2>Learning Html from Scratch</h2>
+		</div>
+		<div class="content2">
+			<p>Explore the power of html in web development</p>
+		</div>
+
+
+
+		<div class="stars">
+
+			<div class="stars2">
+				<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			</div>
+
+			<div class="stars3">
+				<p>Get Started</p>
+				<a href="/pro/home.php">
+					<i class="fa fa-arrow-circle-right" style="color:#F7931E"></i>
+				</a>
+			</div>
+		</div>
+
+
+	</div>
+
+
+		<div class="box">
+		<div class="boximg">
+			<img src="media/learningphp2.jpg">
+		</div>
+		<div class="content">
+			<h2>Learning Html from Scratch</h2>
+		</div>
+		<div class="content2">
+			<p>Explore the power of html in web development</p>
+		</div>
+
+
+
+		<div class="stars">
+
+			<div class="stars2">
+				<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			</div>
+
+			<div class="stars3">
+				<p>Get Started</p>
+				<a href="/pro/home.php">
+					<i class="fa fa-arrow-circle-right" style="color:#F7931E"></i>
+				</a>
+			</div>
+		</div>
+
+
+	</div>
+
+
+		<div class="box">
+		<div class="boximg">
+			<img src="media/learningmysql3.jpg">
+		</div>
+		<div class="content">
+			<h2>Learning Html from Scratch</h2>
+		</div>
+		<div class="content2">
+			<p>Explore the power of html in web development</p>
+		</div>
+
+
+
+		<div class="stars">
+
+			<div class="stars2">
+				<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			</div>
+
+			<div class="stars3">
+				<p>Get Started</p>
+				<a href="/pro/home.php">
+					<i class="fa fa-arrow-circle-right" style="color:#F7931E"></i>
+				</a>
+			</div>
+		</div>
+
+
+	</div>
+	<div class="secheads2">
+	<h2>And Much More</h2>
+</div>
+
+</div>
+
+</section>
+
+<div class="subheads">
+	<h1>Testimonials</h1>
+</div>
+<section class="sec6">
+	<div class="box2">
+		<div class="stars4">
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+		</div>
+		<div class="info2">
+			<p>They did a great job in the process. After 4 unsuccessful trials from other agencies, I was recommended to Wiseway Immigration and few months Later, I am already in Canada. Thank you Wiseway Immigration.</p>
+		</div>
+		<div class="profile">
+		<img src="media/learningmysql3.jpg">
+		<h2>Jessica James</h2>
+		</div>
+	</div>
+
+
+		<div class="box2">
+		<div class="stars4">
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+		</div>
+		<div class="info2">
+			<p>They did a great job in the process. After 4 unsuccessful trials from other agencies, I was recommended to Wiseway Immigration and few months Later, I am already in Canada. Thank you Wiseway Immigration.</p>
+		</div>
+		<div class="profile">
+		<img src="media/learningmysql3.jpg">
+		<h2>Jessica James</h2>
+		</div>
+	</div>
+
+
+		<div class="box2">
+		<div class="stars4">
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+		</div>
+		<div class="info2">
+			<p>They did a great job in the process. After 4 unsuccessful trials from other agencies, I was recommended to Wiseway Immigration and few months Later, I am already in Canada. Thank you Wiseway Immigration.</p>
+		</div>
+		<div class="profile">
+		<img src="media/learningmysql3.jpg">
+		<h2>Jessica James</h2>
+		</div>
+	</div>
+
+
+		<div class="box2">
+		<div class="stars4">
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+			<i class="fas fa-star " style="color: #FDE165"></i>
+		</div>
+		<div class="info2">
+			<p>They did a great job in the process. After 4 unsuccessful trials from other agencies, I was recommended to Wiseway Immigration and few months Later, I am already in Canada. Thank you Wiseway Immigration.</p>
+		</div>
+		<div class="profile">
+		<img src="media/learningmysql3.jpg">
+		<h2>Jessica James</h2>
+		</div>
+	</div>
+</section>
+
+<footer id="contact">
+<div class='foot1'>
+<p class='followhead'>Contact</p>
+<p class='follow'>
+Phone: 08052828424
+</p>
+<p class='follow'>
+Email: stephcomdata@gmail.com
+</p>
+<p class='follow'>
+Address: Yelwa-Tudu, Bauchi State,Nigeria
+<p>
+</div>
+<div class='foot2'>
+<p class='followhead'>Contact</p>
+<a href='/home'>Home</a><br>
+<a href='/home'>About Us</a><br>
+<a href='/home'>Services</a><br>
+<a href='/home'>Blog</a><br>
+<a href='/home'>Contact</a><br>
+</div>
+</footer>
+</div>
+</body>
+
+
+<script type="text/javascript">
+$('.slide').slideUp(10);
+
+$('.bar').click(function(){
+	$('.slide').slideDown(80);
+    $('.slide').css('display','flex');
+	$('.bar').css('display','none');
+	$('.bar2').css('display','block')
+	$('.nav').css('left','-250px')
+})
+
+$('.bar2').click(function(){
+	$('.slide').slideUp(80);
+	$('.bar2').css('display','none');
+	$('.bar').css('display','block')
+		$('.nav').css('left','0px')
+})
+
+</script>
+
+</html>
